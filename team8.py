9cf31b1de2884a25b0cab3ae8528becab6dff818
Richard Lee <547924@my.cuhsd.org>
2019-11-19 11:27:24 -0800

    First collude scenario
    always collude at first time, then betray no matter what

--- team8.py
+++ team8.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'We luv Mostafa' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'First collude'
+strategy_description = 'Colludes together at first, then keep betraying'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,7 +26,10 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    if len(my_history)==0:
+        return 'c'
+    else:
+        return 'b'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
